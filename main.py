e132d773ff8b4a65c915e814b08ee2d19de5982a
cuboids <44651818+cuboids@users.noreply.github.com>
2021-11-25 21:45:28 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import itertools
 
 import numpy as np
+import numpy.random as npr
 import scipy
 from scipy.sparse import csr_matrix
 from scipy.spatial import distance
@@ -10,15 +11,22 @@ from scipy.spatial import distance
 JS_SIMILARITY_THRESHOLD = .5
 CS_SIMILARITY_THRESHOLD = .73
 DCS_SIMILARITY_THRESHOLD = .73
+SEED = 1
 
 # Hyperparemeters (tunable)
 SIGNATURE_LENGTH = 128
+N_BANDS = 16
 
 
 # For now... eventually we will allow
 # flags that specify the file path as
 # required.
 umr = np.load('user_movie_rating.npy')
+umr[:, :2] -= 1  # Ensure rows and columns start at 0
+
+# We probably don't even need these helper functions:
+# we can directly use scipy.distance to reduce the
+# number of function invocation (read: overhead).
 
 
 def jaccard_similarity(u1, u2):
@@ -38,13 +46,26 @@ def discrete_cosine_similarity(u1, u2):
     return distance.cosine(u1, u2)
 
 
-def jaccard_main():
-    """Find user pairs u1 and u2 such that JS(u1, u2) > JS_SIMILARITY_THRESHOLD"""
-    # note: the .sign() performs truncation for us.
-    input_matrix = csr_matrix((umr[:, 2], (umr[:, 0], umr[:, 1]))).sign()
-    signature_matrix = np.empty([SIGNATURE_LENGTH, input_matrix.shape[1]])
+def jaccard_main(toy=None):
+    """Find user pairs u1 and u2 such that JS(u1, u2) > JS_SIMILARITY_THRESHOLD
+
+    toy: reducing the data"""
+
+    input_matrix = csr_matrix((umr[:toy, 2], (umr[:toy, 1], umr[:toy, 0]))).sign()
+    signature_matrix = np.zeros([SIGNATURE_LENGTH, input_matrix.shape[1]], dtype=int)
 
     # Minhashing to fill the signature matrix
+    rng = npr.default_rng(SEED)
+    m = input_matrix.copy()
+    indices = list(range(input_matrix.shape[0]))
+    for i in range(SIGNATURE_LENGTH):
+        rng.shuffle(indices)
+        m = m[indices, :]
+        signature_matrix[i, :] = m.argmax(0)
+
+    print(signature_matrix)
+    print(signature_matrix.shape)
+
     ...
 
     # Banding the signature matrix
@@ -65,3 +86,5 @@ def main():
 
 if __name__ == '__main__':
     main()
+    jaccard_main(10000)
+
