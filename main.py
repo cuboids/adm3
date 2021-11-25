77bbc5eab28b2eaf051d379427291caa0ccbc86c
cuboids <44651818+cuboids@users.noreply.github.com>
2021-11-25 18:13:25 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,47 @@
 import numpy as np
 import scipy
 from scipy.sparse import csr_matrix
+from scipy.spatial import distance
 
 
 # For now... eventually we will allow
 # flags that specify the file path as
 # required.
-data = np.load('user_movie_rating.npy')
+user_movie_rating = np.load('user_movie_rating.npy')
 
-# Trying to figure out how to make the CSR...
-# m = csr_matrix(data[:1000, 2], (data[:1000, 0], data[:1000, 1]))
-# print(m)
+# Getting a feeling for CSR :)
+user_movie = user_movie_rating.copy()
+user_movie[:, 2] = 1
+m = csr_matrix((user_movie[:, 2], (user_movie[:, 0], user_movie[:, 1])))
+print(m)
 
 
 def jaccard_similarity(u1, u2):
-    pass
+    return distance.jaccard(u1, u2)
+
+
+def test_jaccard_similarity(u1, u2)
+    assert jaccard_similarity(u1, u2) == len(np.intersect1d(u1, u2))/len(np.union1d(u1, u2))
 
 
 def cosine_similarity(u1, u2):
+    return distance.cosine(u1, u2)
+
+
+# Same as cosine similarity, just with truncated u1 and u2
+def discrete_cosine_similarity(u1, u2):
+    return distance.cosine(u1, u2)
+
+
+def jaccard_main():
     pass
 
 
-def discrete_cosine_similarity(u1, u2):
+def cosine_main():
+    pass
+
+
+def discrete_cosine_main():
     pass
 
 
