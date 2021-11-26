4e03e5018c9c1b0c834fd5cc6e75338990d1dd58
cuboids <44651818+cuboids@users.noreply.github.com>
2021-11-26 18:39:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import time
 import numpy as np
 import numpy.random as npr
 from scipy.sparse import csr_matrix
+from scipy.spatial import distance
 
 
 class LSH:
@@ -14,9 +15,8 @@ class LSH:
     _CS_THRESHOLD = .73
     _DCS_THRESHOLD = .73
 
-    _SIGNATURE_LENGTH = 128
-    _N_ROWS_PER_BAND = 8
-    _N_BANDS = _SIGNATURE_LENGTH // _N_ROWS_PER_BAND
+    _SIGNATURE_LENGTH = 140
+    _N_ROWS_PER_BAND = 14
 
     def __init__(self, fp, seed=0):
         # TODO: allow fp to be specified by flag
@@ -36,28 +36,13 @@ class LSH:
         self._input_matrix = csr_matrix((self._umr[:, 2], (self._umr[:, 1], self._umr[:, 0])))
         self._input_matrix_truncated = self._input_matrix.sign()
 
-    def _jaccard_main(self, verbose=True):
-        """Find user pairs (u1, u2) such that JS(u1, u2) < JS_THRESHOLD"""
+    @property
+    def _N_BANDS(self):
+        return self._SIGNATURE_LENGTH // self._N_ROWS_PER_BAND
 
-        t0 = time.time()
-        # We will first construct the signature matrix
-        signature_matrix = np.full(([self._SIGNATURE_LENGTH, self._input_matrix.shape[1]]), np.inf)
-
-        # Permute the rows of m SIGNATURE_LENGTH times
-        rng = npr.default_rng(self._seed)
-        m = self._input_matrix.copy()
-        indices = list(range(m.shape[0]))
-        for i in range(self._SIGNATURE_LENGTH):
-            rng.shuffle(indices)
-            m = m[indices, :]
-            signature_matrix[i, :] = m.argmax(0)
-
-        # Make sure the whole input matrix is filled
-        assert np.isfinite(signature_matrix.all())
-        t1 = time.time()
-
-        if verbose:
-            print(f'minhashing time: {t1 - t0:.2f}')
+    def _compute_blocked_pairs(self, signature_matrix):
+        """Compute the blocked pairs of a signature matrix Σ
+        given a distance measure"""
 
         # Banding the signature matrix
         hash_buckets = defaultdict(set)
@@ -79,13 +64,39 @@ class LSH:
             for pair in itertools.combinations(bucket, 2):
                 blocked_pairs.add(pair)
 
+        return blocked_pairs
+
+    def _jaccard_main(self, verbose=True):
+        """Find user pairs (u1, u2) such that JS(u1, u2) < JS_THRESHOLD"""
+
+        t0 = time.time()
+        # We will first construct the signature matrix
+        signature_matrix = s = np.full(([self._SIGNATURE_LENGTH, self._input_matrix.shape[1]]), np.inf)
+
+        # Permute the rows of m SIGNATURE_LENGTH times
+        rng = npr.default_rng(self._seed)
+        m = self._input_matrix_truncated.copy()
+        indices = list(range(m.shape[0]))
+        for i in range(self._SIGNATURE_LENGTH):
+            rng.shuffle(indices)
+            m = m[indices, :]
+            signature_matrix[i, :] = m.argmax(0)
+
+        # Make sure the whole input matrix is filled
+        assert np.isfinite(signature_matrix.all())
+        t1 = time.time()
+
+        if verbose:
+            print(f'minhashing time: {t1 - t0:.2f}')
+
+        blocked_pairs = self._compute_blocked_pairs(signature_matrix)
+
         # TODO: parallelize
         # Calculate the candidate pairs, that is, the pairs for
         # which JS(s1, s2) > JS_THRESHOLD for signatures s1 and s2
         # corresponding to u1 and u2 respectively
         t2 = time.time()
         candidate_pairs = set()
-        s = signature_matrix.copy()
         for u1, u2 in blocked_pairs:
             if np.sum(s[:, u1] == s[:, u2]) / self._SIGNATURE_LENGTH >= self._JS_THRESHOLD:
                 candidate_pairs.add((u1, u2))
@@ -99,13 +110,53 @@ class LSH:
                 pairs.add((u1, u2))
         t3 = time.time()
         if verbose:
-            print(f'JS calculation time: {(t3 - t2):.2f}')
+            print(f'JS calculation time: {t3 - t2:.2f}')
             print(f'Number of pairs found: {len(pairs)}')
 
         # TODO: Update result.txt for each pair in pairs
 
+    def _cosine_main(self, verbose=True):
+        # WARNING: not working correctly yet.
+        # Possible factor scipy.distance is not suitable.
+
+        t0 = time.time()
+
+        # Use random hyperplanes to create signatures
+        rng = npr.default_rng(self._seed)
+        m = self._input_matrix.copy()
+
+        # Create random matrix
+        hyperplanes = 2 * rng.integers(2, size=(m.shape[0], self._SIGNATURE_LENGTH)) - 1
+        signature_matrix = s = np.sign(m.T.dot(hyperplanes)).T
+
+        t1 = time.time()
+        if verbose:
+            print(f'Signature calculation time: {t1 - t0:.2f}')
+
+        blocked_pairs = self._compute_blocked_pairs(signature_matrix)
+
+        # TODO: parallelize
+        # Calculate the candidate pairs, that is, the pairs for
+        # which CS(s1, s2) > CS_THRESHOLD for signatures s1 and s2
+        # corresponding to u1 and u2 respectively
+        t2 = time.time()
+        candidate_pairs = set()
+        for u1, u2 in blocked_pairs:
+            if 1 - distance.cosine(s[:, u1], s[:, u2]) >= self._CS_THRESHOLD:
+                candidate_pairs.add((u1, u2))
+
+        # Calculate the actual cosine distance for the candidate pairs
+        pairs = set()
+        for u1, u2 in candidate_pairs:
+            if 1 - distance.cosine(m.getcol(u1).toarray(), m.getcol(u2).toarray()) >= self._CS_THRESHOLD:
+                pairs.add((u1, u2))
+        t3 = time.time()
+        if verbose:
+            print(f'JS calculation time: {t3 - t2:.2f}')
+            print(f'Number of pairs found: {len(pairs)}')
+
     def main(self):
-        self._jaccard_main()
+        self._cosine_main()
 
 
 def main():
@@ -118,5 +169,27 @@ if __name__ == '__main__':
 
 
 # Output for with toy=None:
-# >>> minhashing time: 159.16
-# >>> JS calculation time: ????.??
+# >>> minhashing time: ~159.16
+# >>> JS calculation time: fast!
+
+# Console script to manipulate signature matrix in console
+"""
+from collections import defaultdict
+import itertools
+import time
+
+import numpy as np
+import numpy.random as npr
+from scipy.sparse import csr_matrix
+from scipy.spatial import distance
+
+SIGNATURE_LENGTH = 128
+fp = 'user_movie_rating.npy'
+umr = np.load(fp)
+umr[:, :2] -= 1
+m = csr_matrix((umr[:, 2], (umr[:, 1], umr[:, 0])))
+rng = npr.default_rng(0)
+hyperplanes = 2 * rng.integers(2, size=(m.shape[0], SIGNATURE_LENGTH)) - 1
+s = m.T.dot(hyperplanes)
+print(s)
+"""
