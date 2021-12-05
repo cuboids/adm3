943dfc55b23d9438a927a3bbedc035a8219c2988
cuboids <44651818+cuboids@users.noreply.github.com>
2021-12-05 13:05:46 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
+import numpy as np
+import pandas as pd
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
+import xgboost
+
+"""
+Steps:
+
+* csv -> pandas dataframe
+
+* basic data cleanup
+
+* target = disposition using kepler data
+
+* SVM
+
+* Random Forests
+
+* XGBoost
+
+* Dimension reduction and data viz
+
+"""
+
 pass
+
+
+if __name__ == '__main__':
+    pass
